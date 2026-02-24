615cc5d656e5341ff572715f2bfe59c43844b273
CoodeNoob <swanhtetpyaesone323@gmail.com>
2026-02-24 13:54:14 +0630

    add_cors

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 //ROUTES
 const authRoutes = require('./src/routes/auth.route');
 const StudentRoutes = require('./src/routes/student.route');
+
+
+
+//CORS OPTIONS
+const allowOrigin = [
+    'http://localhost:3000'
+];
+
+
+const corsOptions = {
+    fn: (origin, callback) => {
+        // allow requests with no origin (like mobile apps, ESP8266, Postman)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}
+
 
 app.use(express.json());
 
@@ -13,6 +36,13 @@ app.use(
     '/uploads',
     express.static(path.join(__dirname, 'src', 'uploads'))
 );
+app.use(cors({
+    origin: corsOptions.fn,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
@@ -33,7 +63,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/student', StudentRoutes);
 
-
-
-
-module.exports = app;
+module.exports = app;   
